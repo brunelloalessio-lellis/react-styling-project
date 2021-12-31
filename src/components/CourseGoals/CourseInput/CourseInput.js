babb826e8ccca23263380f0c2e1db475e02b7378
brunelloalessio-lellis <brunelloalessioandroid@gmail.com>
2021-12-31 08:43:46 +0100

    used style to alert user that intered invalid data

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,25 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true)
 
-  const goalInputChangeHandler = event => {
+  const goalInputChangeHandler = (event) => {
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
-    props.onAddGoal(enteredValue);
+
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false)
+      return;
+    } else {
+      props.onAddGoal(enteredValue);
+    }
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{
+          color: !isValid ? 'red':'initial'
+        }}>Course Goal</label>
+        <input style={{
+          color: !isValid ? 'red':'initial',
+          backgroundColor: !isValid ? 'red':'initial'
+        }} type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
